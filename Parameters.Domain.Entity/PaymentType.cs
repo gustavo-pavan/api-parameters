3ec672c6b028d51047562db8c68d43e1efc9ded0
gustavo-pavan <Gustavo Pavan@DESKTOP-D9SACH7>
2023-04-28 14:56:35 -0300

    refactor test

---
+++ Parameters.Domain.Entity/PaymentType.cs
@@ -0,0 +1,24 @@
+﻿namespace Parameters.Domain.Entity;
+
+public class PaymentType : BaseEntity
+{
+    public PaymentType(string name, string description)
+    {
+        if (string.IsNullOrEmpty(name) || string.IsNullOrWhiteSpace(name))
+            throw new ArgumentException($"{nameof(Name)} can't be null or empty"); 
+        
+        if (string.IsNullOrEmpty(description) || string.IsNullOrWhiteSpace(description))
+            throw new ArgumentException($"{nameof(Description)} can't be null or empty");
+
+        Name = name;
+        Description = description;
+    }
+
+    public PaymentType(Guid id, string name, string description) : this(name, description)
+    {
+        Id = id;
+    }
+
+    public string Name { get; private set; }
+    public string Description { get; private set; }
+}
--- Parameters.Test.Unit/Domain/PaymentTypeTest.cs
+++ Parameters.Test.Unit/Domain/PaymentTypeTest.cs
@@ -35,7 +35,7 @@ public class PaymentTypeTest
     public void Should_Throw_Exception_When_Name_Is_Invalid(string name)
     {
         var action = () => new PaymentType(_dynamic?.Id, name, _dynamic?.Description);
-        action.Should().Throw<ArgumentException>().WithMessage($"{nameof(FlowParameter.Name)} can't be null or empty");
+        action.Should().Throw<ArgumentException>().WithMessage($"{nameof(PaymentType.Name)} can't be null or empty");
     }
 
     [Theory]
@@ -45,29 +45,6 @@ public class PaymentTypeTest
     public void Should_Throw_Exception_When_Description_Is_Invalid(string description)
     {
         var action = () => new PaymentType(_dynamic?.Id, _dynamic?.Name, description);
-        action.Should().Throw<ArgumentException>().WithMessage($"{nameof(FlowParameter.Description)} can't be null or empty");
+        action.Should().Throw<ArgumentException>().WithMessage($"{nameof(PaymentType.Description)} can't be null or empty");
     }
-}
-
-public class PaymentType : BaseEntity
-{
-    public PaymentType(string name, string description)
-    {
-        if (string.IsNullOrEmpty(name) || string.IsNullOrWhiteSpace(name))
-            throw new ArgumentException($"{nameof(Name)} can't be null or empty"); 
-        
-        if (string.IsNullOrEmpty(description) || string.IsNullOrWhiteSpace(description))
-            throw new ArgumentException($"{nameof(Description)} can't be null or empty");
-
-        Name = name;
-        Description = description;
-    }
-
-    public PaymentType(Guid id, string name, string description) : this(name, description)
-    {
-        Id = id;
-    }
-
-    public string Name { get; private set; }
-    public string Description { get; private set; }
 }

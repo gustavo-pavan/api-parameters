ca396fd910638fad481d54859817ac8a6e998100
gustavo-pavan <Gustavo Pavan@DESKTOP-D9SACH7>
2023-04-29 14:44:04 -0300

    add domain events

--- Parameters.Domain.Entity/BaseEntity.cs
+++ Parameters.Domain.Entity/BaseEntity.cs
@@ -1,6 +1,27 @@
-﻿namespace Parameters.Domain.Entity;
+﻿using MediatR;
+
+namespace Parameters.Domain.Entity;
 
 public abstract class BaseEntity
 {
     public Guid Id { get; protected set; }
+
+    private List<INotification>? _domainEvents;
+    public IReadOnlyCollection<INotification>? DomainEvents => _domainEvents?.AsReadOnly();
+
+    public void AddDomainEvent(INotification eventItem)
+    {
+        _domainEvents ??= new List<INotification>();
+        _domainEvents.Add(eventItem);
+    }
+
+    public void RemoveDomainEvent(INotification eventItem)
+    {
+        _domainEvents?.Remove(eventItem);
+    }
+
+    public void ClearDomainEvents()
+    {
+        _domainEvents?.Clear();
+    }
 }
